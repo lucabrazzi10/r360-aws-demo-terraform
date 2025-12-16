41c98da69ea1c29c26232bb7027ef7a4a9a244dd
abayomi folarin <folarin.abayomi01@gmail.com>
2025-12-16 11:49:25 -0600

    Add Terraform apply/destroy workflows with AWS OIDC

--- envs/primary/variables.tf
+++ envs/primary/variables.tf
@@ -1,29 +1,21 @@
 variable "name_prefix" {
-  type        = string
-  description = "Name prefix for primary environment"
-  default     = "r360-primary"
+  type = string
 }
 
 variable "aws_region" {
-  type        = string
-  description = "AWS region"
-  default     = "us-east-1"
+  type = string
 }
 
-variable "db_master_username" {
-  type        = string
-  description = "DB master username"
-  default     = "rgs"
+variable "db_username" {
+  type = string
+  sensitive = true
 }
 
-variable "db_master_password" {
-  type        = string
-  description = "DB master password"
-  sensitive   = true
+variable "db_password" {
+  type = string
+  sensitive = true
 }
 
 variable "app_instance_profile" {
-  type        = string
-  description = "Optional IAM instance profile name for app instances"
-  default     = ""
+  type = string
 }
