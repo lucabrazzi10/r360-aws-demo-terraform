c996bb71115e57539ce66a0e1473d3a9243d1c30
abayomi folarin <folarin.abayomi01@gmail.com>
2025-12-16 12:10:45 -0600

    Fix destroy workflow path evaluation order

--- envs/primary/variables.tf
+++ envs/primary/variables.tf
@@ -1,21 +1,22 @@
-variable "name_prefix" {
-  type = string
-}
-
 variable "aws_region" {
   type = string
 }
 
-variable "db_username" {
+variable "name_prefix" {
   type = string
+}
+
+variable "db_master_username" {
+  type      = string
   sensitive = true
 }
 
-variable "db_password" {
-  type = string
+variable "db_master_password" {
+  type      = string
   sensitive = true
 }
 
 variable "app_instance_profile" {
-  type = string
+  type    = string
+  default = ""
 }
