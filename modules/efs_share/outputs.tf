d318d765bc90061b2a3a07a7e196af3905dd5a2d
abayomi folarin <folarin.abayomi01@gmail.com>
2025-12-16 11:53:23 -0600

    Fix EFS module outputs (efs_id, dns_name)

--- modules/efs_share/outputs.tf
+++ modules/efs_share/outputs.tf
@@ -1,11 +1,20 @@
-output "efs_dns_name" {
-  value = aws_efs_file_system.efs.dns_name
-}
+# output "efs_dns_name" {
+#   value = aws_efs_file_system.efs.dns_name
+# }
 
-output "efs_sg_id" {
-  value = aws_security_group.efs_sg.id
-}
+# output "efs_sg_id" {
+#   value = aws_security_group.efs_sg.id
+# }
+
+# output "efs_id" {
+#   value = aws_efs_file_system.efs.id
+# }
+
 
 output "efs_id" {
   value = aws_efs_file_system.efs.id
+}
+
+output "dns_name" {
+  value = aws_efs_file_system.efs.dns_name
 }
